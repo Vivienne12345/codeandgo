507656a6207ba9df80f67fba8d0029eb584f3d2b
Vivienne12345 <jerems5@proton.me>
2024-05-09 19:39:37 +0200

    Delete src directory

--- src/components/links.ts
+++
@@ -1,7 +0,0 @@
-export const links = {
-  email: "mailto:contact@codeandgo.fr",
-  emailId:"contact@codeandgo.fr",
-  twitter: "https://twitter.com/elonmusk",
-  linkedin: "https://www.linkedin.com/in/ryanroslansky/",
-  instagram: "https://www.instagram.com/zuck/",
-};
